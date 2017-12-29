3cb97e31b3f69f801cc532b74375be5c45dffdce
nucleos <qwer>
2017-12-29 19:16:34 +0200

    Lesson04 - (Part01)

---
+++ Pro004_Examples/src/RandomOrg.java
@@ -0,0 +1,9 @@
+public class RandomOrg {
+
+    public static void main(String[] args) {
+        int [] arr = {7, 99, 98, 66, 24, 11, 13, 15, 27, 28, 55, 77, 5, 4};
+        int a = (int)(Math.random()*14);
+        System.out.println("Victory: " + arr[a]);
+    }
+
+}
