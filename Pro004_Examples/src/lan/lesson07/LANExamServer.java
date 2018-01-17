18e884d4cfadabcf2408e09dbab9e733ac4bc152
nucleos <qwer>
2018-01-17 20:17:36 +0200

    LAN

---
+++ Pro004_Examples/src/lan/lesson07/LANExamServer.java
@@ -0,0 +1,45 @@
+package lan.lesson07;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class LANExamServer {
+
+    /*Сервер
+    *
+    * 1 - 1029 - порты которые не следует использовать в
+    *            сокетах, так как они являются системными
+    * 8081 - 65536 - безопасные для использования порты.*/
+
+    public static void main(String[] args) throws IOException {
+        /* ServerSocketChannel - позволяет нам запустить соедение
+        * и начать прослушивать порт*/
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        /*Порт - необходим для мониторинга подключения клиентов
+        *        и установки соеденения с ними.*/
+        ssc.bind(new InetSocketAddress(30000));
+
+        System.out.println("Режим ожидания соеденения... ");
+
+        SocketChannel client = ssc.accept();
+
+        System.out.println("Установка соеденения с клиентом...");
+
+        ByteBuffer bb = ByteBuffer.allocate(128);
+
+        int bytes = 0;
+        while ((bytes = client.read(bb)) > 0) {
+            bb.flip();
+            System.out.println("Входящее сообщение: "
+                + new String(bb.array(), 0, bytes));
+            bb.clear();
+        }
+
+        System.out.println("Завершение работы сервера....");
+
+
+    }
+}
